63eb49924dd1f35101d546d9a05e639252998517
ClaraDianAnggraini <149463323+ClaraDianAnggraini@users.noreply.github.com>
2024-04-04 08:14:05 +0700

    Latihan 1

---
+++ contoh1.php
@@ -0,0 +1,13 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo"Nama saya Clara Dian Anggraini
+            Saya tinggal di daerah Johar Baru
+            Olahraga yang saya sukai adalah
+            Bulutangkis dan Basket";
+ }
+}
+?>
